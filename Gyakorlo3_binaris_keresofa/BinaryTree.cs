311795a479dbd61ceaeb7c0577f5a302f7954a8f
Sindel Szabolcs <sindelsz@griffsoft.hu>
2018-09-14 16:11:49 +0200

    first commit

---
+++ Gyakorlo3_binaris_keresofa/BinaryTree.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakorlo3_binaris_keresofa
+{
+    class BinaryTree
+    {
+
+        private Node rootNode;
+
+        public BinaryTree()
+        {
+            rootNode = null;
+        }
+
+        public void Add(int value)
+        {
+            if (rootNode == null)
+            {
+                rootNode = new Node(value, null);
+            }
+            else
+            {
+                Node currentNode = rootNode;
+                Node parentNode = null;
+                
+                while(currentNode != null)
+                {
+                    if (value < currentNode.Key)
+                    {
+                        parentNode = currentNode;
+                        currentNode = currentNode.LeftNode;
+                    }
+                    else if (value > currentNode.Key)
+                    {
+                        parentNode = currentNode;
+                        currentNode = currentNode.RightNode;
+                    }
+                    else
+                    {   
+                        return;
+                    }
+                }
+
+                currentNode = new Node(value, parentNode);
+
+                if (value < parentNode.Key)
+                {
+                    parentNode.LeftNode = currentNode;
+                }
+                else
+                {
+                    parentNode.RightNode = currentNode;
+                }
+            }
+        }
+
+
+        public bool Contains(int value)
+        {
+            Node currentNode = rootNode;
+
+
+            while (currentNode != null)
+            {
+                if (currentNode.Key == value)
+                {
+                    return true;
+                }
+                else if (value < currentNode.Key)
+                {
+                    currentNode = currentNode.LeftNode;
+                }
+                else
+                {
+                    currentNode = currentNode.RightNode;
+                }
+            }
+
+            return false;
+
+        }
+
+
+        
+        class Node{
+
+            public Node ParentNode { get; set; }
+            public Node LeftNode { get; set; }
+            public Node RightNode { get; set; }
+
+            public int Key { get; set; }
+
+            public Node(int key, Node parent)
+            {
+                this.Key = key;
+                this.ParentNode = parent;
+            }
+
+        }
+
+
+    }
+}
---
+++ Gyakorlo3_binaris_keresofa/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakorlo3_binaris_keresofa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BinaryTree bTree = new BinaryTree();
+            bTree.Add(10);
+            bTree.Add(90);
+            bTree.Add(35);
+            bTree.Add(-20);
+            bTree.Add(70);
+
+            Console.WriteLine("Tree containts {0}: {1}", 10, bTree.Contains(10));
+            Console.WriteLine("Tree containts {0}: {1}", 20, bTree.Contains(20));
+            Console.WriteLine("Tree containts {0}: {1}", 35, bTree.Contains(35));
+            Console.WriteLine("Tree containts {0}: {1}", 90, bTree.Contains(90));
+            Console.WriteLine("Tree containts {0}: {1}", -100, bTree.Contains(-100));
+            Console.WriteLine("Tree containts {0}: {1}", -20, bTree.Contains(-20));
+            Console.WriteLine("Tree containts {0}: {1}", 70, bTree.Contains(70));
+
+
+            Console.ReadKey();
+
+        }
+
+
+
+    }
+}
